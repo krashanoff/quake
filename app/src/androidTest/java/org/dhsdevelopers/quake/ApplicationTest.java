81a2fab872cd343145b3737742c1484eb9a0130f
krashanoff <lkrashanoff2@gmail.com>
2015-11-19 16:10:51 -0800

    Add notification within app/src/androidTest/ApplicationTest.java

--- app/src/androidTest/java/org/dhsdevelopers/quake/ApplicationTest.java
+++ app/src/androidTest/java/org/dhsdevelopers/quake/ApplicationTest.java
@@ -3,6 +3,8 @@ package org.dhsdevelopers.quake;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+//THIS IS A TEST FILE THAT ANDROID STUDIO USES.  DO NOT MODIFY.
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
