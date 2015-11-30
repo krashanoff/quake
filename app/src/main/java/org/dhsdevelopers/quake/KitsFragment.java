17010dcdc9211815b532da7d56fba29f92a037c3
krashanoff <lkrashanoff2@gmail.com>
2015-11-29 21:59:15 -0800

    Create a preliminary tab .xml and .java file

---
+++ app/src/main/java/org/dhsdevelopers/quake/KitsFragment.java
@@ -0,0 +1,26 @@
+package org.dhsdevelopers.quake;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Leonid on 11/29/2015.
+ */
+public class KitsFragment extends Fragment{
+    public static KitsFragment newInstance() {
+        KitsFragment fragment = new KitsFragment();
+        return fragment;
+    }
+    public KitsFragment() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.kits_fragment, container, false);
+        return rootView;
+    }
+}
