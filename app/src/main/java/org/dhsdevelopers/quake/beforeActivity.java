0774805230f84dd5cda95be59a0770a63545a4c3
krashanoff <lkrashanoff2@gmail.com>
2015-11-29 21:24:36 -0800

    Remove prefab buttons, various folder exclusions that include bugs

--- app/src/main/java/org/dhsdevelopers/quake/beforeActivity.java
+++ app/src/main/java/org/dhsdevelopers/quake/beforeActivity.java
@@ -52,16 +52,6 @@ public class beforeActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
