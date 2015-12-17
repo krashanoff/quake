afaf27bb2cccef5e5794279b5f6b864fd2c1a948
Duncan Grubbs <duncan.grubbs@gmail.com>
2015-12-17 11:53:20 -0800

    Layout changes.

--- app/src/main/java/org/dhsdevelopers/quake/afterActivity.java
+++ app/src/main/java/org/dhsdevelopers/quake/afterActivity.java
@@ -1,13 +1,14 @@
 package org.dhsdevelopers.quake;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class afterActivity extends AppCompatActivity {
@@ -29,29 +30,15 @@ public class afterActivity extends AppCompatActivity {
         Button friends = (Button) findViewById(R.id.friends);
         Button medical = (Button) findViewById(R.id.medical);
 
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent before = new Intent(afterActivity.this, MapsActivity.class);
+                startActivity(before);
+            }
+        });
 
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_after, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
