555a28e28021b568b5c45dc72190c3c148c62d9d
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-01-04 14:17:03 -0800

    Fix some previous problems.

--- app/src/main/java/quake/dhsdevelopers/org/quake/AfterActivity.java
+++ app/src/main/java/quake/dhsdevelopers/org/quake/AfterActivity.java
@@ -2,8 +2,12 @@ package quake.dhsdevelopers.org.quake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.hardware.Camera;
 
 public class AfterActivity extends AppCompatActivity {
+
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +15,17 @@ public class AfterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_after);
         setTitle("What's Next");
     }
+
+    /* camera = Camera.open();
+
+       flashlight.setOnClickListener(new View.OnClickListener(){ 
+        @Override 
+        public void onClick(View v){ 
+            Camera.Parameters cameraParams = camera.getParameters(); 
+            cameraParams.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH); 
+            camera.setParameters(cameraParams); 
+            camera.startPreview(); 
+        } 
+    }); */
+
 }
