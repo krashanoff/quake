d27e8b0765116210f1da84edaeb16309911803cd
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-01-11 19:20:29 -0800

    Tiny changes.

--- app/src/main/java/quake/dhsdevelopers/org/quake/MainActivity.java
+++ app/src/main/java/quake/dhsdevelopers/org/quake/MainActivity.java
@@ -4,7 +4,7 @@ package quake.dhsdevelopers.org.quake;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;;
+import android.view.View;
 
 //Async
 import android.os.AsyncTask;
