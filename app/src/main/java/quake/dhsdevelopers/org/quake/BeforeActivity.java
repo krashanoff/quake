9d952b24949d4b1e59ce2765b1360e29695d5216
duncangrubbs <duncan.grubbs@gmail.com>
2016-01-15 11:55:07 -0800

    More locaiton changes.

--- app/src/main/java/quake/dhsdevelopers/org/quake/BeforeActivity.java
+++ app/src/main/java/quake/dhsdevelopers/org/quake/BeforeActivity.java
@@ -34,54 +34,4 @@ public class BeforeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /*locationManager = (LocationManager) getSystemService(LOCATION_SERVICE); 
-
-    locationListener = new LocationListener(){ 
-        @Override 
-        public void onLocationChanged(Location location) { 
-            text.append("\n"+location.getLatitude() + " " + location.getLatitude()); 
-        }  
-
-        @Override 
-        public void onStatusChanged(String provider, int status, Bundle extras) {  
-        }  
-
-        @Override 
-        public void onProviderEnabled(String provider) {  
-        }  
-
-        @Override 
-        public void onProviderDisabled(String provider) { 
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS); 
-            startActivity(intent); 
-        } 
-    }; 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { 
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) { 
-            requestPermissions(new String[]{ 
-                    Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION, 
-                    Manifest.permission.INTERNET 
-            }, 10); 
-            return; 
-        } 
-    }else{ 
-        configureButton(); 
-    }
-  }
-      public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults){ 
-        switch (requestCode){ 
-        case 10: 
-        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) 
-        configureButton(); 
-        return; 
-        }
-         }
-           public void configureButton(){ 
-        getLocation.setOnClickListener(new View.OnClickListener() { 
-
-        @Override 
-        public void onClick(View v) { 
-        locationManager.requestLocationUpdates("gps", 5000, 3000, locationListener); 
-        } 
-    });*/
 }
--- app/src/main/java/quake/dhsdevelopers/org/quake/LocalActivity.java
+++ app/src/main/java/quake/dhsdevelopers/org/quake/LocalActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.content.Context;
+import android.content.Intent;
 
 public class LocalActivity extends AppCompatActivity {
 
@@ -13,8 +14,29 @@ public class LocalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local);
         setTitle("Local Data");
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
-    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    /* locationListener = new LocationListener(){ 
+        @Override 
+        public void onLocationChanged(Location location) { 
+            text.append("\n"+location.getLatitude() + " " + location.getLatitude()); 
+        }  
+
+        @Override 
+        public void onStatusChanged(String provider, int status, Bundle extras) {  
+        }  
+
+        @Override 
+        public void onProviderEnabled(String provider) {  
+        }  
+
+        @Override 
+        public void onProviderDisabled(String provider) { 
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS); 
+            startActivity(intent); 
+        } 
+    };  */
 
 }
