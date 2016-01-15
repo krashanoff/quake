6ff2b425bb4edf74e10b5091e08e3119d1eb2a61
duncangrubbs <duncan.grubbs@gmail.com>
2016-01-15 12:16:35 -0800

    Add medical info activity.

---
+++ app/src/main/java/quake/dhsdevelopers/org/quake/MedicalInfoActivity.java
@@ -0,0 +1,13 @@
+package quake.dhsdevelopers.org.quake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MedicalInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_info);
+    }
+}
